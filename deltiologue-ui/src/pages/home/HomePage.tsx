86adb589e90c8a8ded21f9d41543a07711ad11e2
Andrew Baxter <rhinocratic@googlemail.com>
2026-01-12 19:46:33 +0000

    SAVEPOINT

--- deltiologue-ui/src/pages/home/HomePage.tsx
+++ deltiologue-ui/src/pages/home/HomePage.tsx
@@ -4,8 +4,6 @@ export default function HomePage() {
   return (
     <div>
       <div>Home Page</div>
-      <div>Domain: {import.meta.env.VITE_AUTH0_DOMAIN}</div>
-      <div>Client ID: {import.meta.env.VITE_AUTH0_CLIENT_ID}</div>
       <ApiCall />
     </div>
   );
